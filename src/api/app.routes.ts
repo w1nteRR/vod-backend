d0365e206b6145047b514d2be0320744a65e05c5
w1nteRR <mykola796@gmail.com>
2021-02-07 13:54:59 +0200

    add endpoints

--- src/api/app.routes.ts
+++ src/api/app.routes.ts
@@ -23,7 +23,62 @@ export default (app: Router) => {
       return api._200(watchlist)
 
     } catch (err) {
-      console.log(err)
+      return api._400(err.message)
+    }
+  })
+
+  router.get('/status', decodeAccessToken, async (req: Request, res: Response) => {
+
+    const { sub } = req.body
+    const filmId = req.query.filmId as string
+
+    const api = new ApiResponses(res)
+    const service = new WatchlistService(sub)
+
+    try {
+      
+      const status = await service.getWatchlistStatus(filmId)
+
+      return api._200({ status })
+
+    } catch (err) {
+      return api._400(err.message)
+    }
+  })
+
+  router.post('/add', decodeAccessToken, async (req: Request, res: Response) => {
+
+    const { sub, filmId } = req.body
+
+    const api = new ApiResponses(res)
+    const service = new WatchlistService(sub)
+
+    try {
+      
+      await service.addFilm(filmId)
+
+      return api._200({ message: 'Success' })
+
+    } catch (err) {
+      return api._400(err.message)
+    }
+  })
+
+  router.delete('/remove', decodeAccessToken, async (req: Request, res: Response) => {
+
+    const { sub } = req.body
+    const filmId = req.query.filmId as string
+
+    const api = new ApiResponses(res)
+    const service = new WatchlistService(sub)
+
+    try {
+      
+      await service.removeFilm(filmId)
+
+      return api._200({ message: 'Success' })
+
+    } catch (err) {
       return api._400(err.message)
     }
   })
